c0fb6eeca1684a125678494cd18815fbf3d14f4a
Eduard Ilyaskin <ilyaskin1998@gmail.com>
2018-02-02 20:36:28 +0600

    bootstrap classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// eject
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// styles
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
