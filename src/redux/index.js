48af0a193e0c8701bfc286b55884e4c394eb3715
Eduard Ilyaskin <ilyaskin1998@gmail.com>
2018-02-02 21:23:32 +0600

    "history": "^4.7.2","react-router-dom": "^4.2.2","react-router-redux": "^4.0.8","redux-logger": "^3.0.6","redux-saga": "^0.16.0",

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import reducers from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+const enhancer =  applyMiddleware(thunk, logger);
+
+const store = createStore(reducers, composeWithDevTools(enhancer));
 window.store = store;
 
 export default store;
